321893675f266dbf42e8ab357179722290cfddf9
Alan <115010227@link.cuhk.edu.cn>
2019-11-19 21:25:16 -0500

    change app.js & index.js for search page

--- food_tracker/public/javascripts/app.js
+++ food_tracker/public/javascripts/app.js
@@ -1,31 +1,61 @@
-var app = angular.module('angularjsNodejsTutorial', []);
+var app = angular.module("FoodTracker", []);
 
-app.controller('homeController', function($scope, $http) {
-  $http({
-    url: '/people',
-    method: 'GET'
-  }).then(res => {
-    console.log("PEOPLE: ", res.data);
-    $scope.people = res.data;
-  }, err => {
-    console.log("People ERROR: ", err);
-  });
-});
+app.controller('searchController', function($scope, $http) {
+  // TODO: Q1
+  // $http({
+  //   url: '/search',
+  //   method: 'GET'
+  // }).then(res => {
+  //   console.log("Search: ", res.data);
+  //   $scope. = res.data;
+  // }, err => {
+  //   console.log("Search ERROR: ", err);
+  // });
 
-app.controller('findFriendsController', function($scope, $http) {
-  $scope.submitLogin = function() {
-    // TODO: Part (3) - Add an HTTP request to this function (see lines 4-12 above for reference)
+  $scope.searchMajorDesc = function(){
     $http({
-      url: '/friends/' + $scope.login,
+      url:'/search/' + $scope.major_desc,
       method: 'GET'
     }).then(res => {
-      console.log("FRIENDS: ", res.data);
-      $scope.friends = res.data;
+      console.log("Search Click: ", res.data);
+      // $scope.major_desc = res.data;
+      $scope.MinorDesc = res.data;
+      // $scope.MinorDesc = res.data;
     }, err => {
-      console.log("Friends ERROR: ", err);
+      console.log("Search Click ERROR: ", err);
     });
   }
 });
+
+
+
+
+// app.controller('homeController', function($scope, $http) {
+//   $http({
+//     url: '/people',
+//     method: 'GET'
+//   }).then(res => {
+//     console.log("PEOPLE: ", res.data);
+//     $scope.people = res.data;
+//   }, err => {
+//     console.log("People ERROR: ", err);
+//   });
+// });
+
+// app.controller('findFriendsController', function($scope, $http) {
+//   $scope.submitLogin = function() {
+//     // TODO: Part (3) - Add an HTTP request to this function (see lines 4-12 above for reference)
+//     $http({
+//       url: '/friends/' + $scope.login,
+//       method: 'GET'
+//     }).then(res => {
+//       console.log("FRIENDS: ", res.data);
+//       $scope.friends = res.data;
+//     }, err => {
+//       console.log("Friends ERROR: ", err);
+//     });
+//   }
+// });
 
 
 // Template for adding a controller
