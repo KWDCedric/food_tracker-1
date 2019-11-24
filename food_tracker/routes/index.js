7e83b97f947b7d95f611e8ee5f7a1d84cab308e2
mistiqve <yixuewendy.feng@gmail.com>
2019-11-24 17:25:55 -0500

    Fix duplicate error in nutrition

--- food_tracker/routes/index.js
+++ food_tracker/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 // var config = require('../db-config.js');
-var config = require('../db-config-aws.js');
+var config = require('../db-config.js');
 
 /* ----- Connects to your mySQL database ----- */
 
