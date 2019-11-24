6aa4d9214bcc8bf86c6f0ff686149ca6750d8fc8
Xuchen Wang <xwang@macalester.edu>
2019-11-24 17:41:53 -0500

    sample query #7

--- sample_query.sql
+++ sample_query.sql
@@ -91,3 +91,47 @@ WHERE T.protein_value > 0.1
 ORDER BY F.description; 
 
 
+-- 7
+var id = req.params.item.fdc_id;
+var amount = req.params.item.amount;
+
+WITH temp_protein AS(
+    SELECT F.fdc_id, FN.amount*(amount/100) AS protein_value, FN.amount * CCF.protein_value *(amount/100) AS protein_calorie
+    FROM food F JOIN food_nutrient FN ON F.fdc_id = FN.fdc_id
+            JOIN nutrient N ON FN.nutrient_id = N.nutrient_id
+            JOIN food_nutrient_conversion_factor NCF ON F.fdc_id = NCF.fdc_id
+            JOIN food_calorie_conversion_factor CCF ON NCF.nutrient_conversion_id = CCF. nutrient_conversion_id
+    WHERE N.name = 'protein' AND F.fdc_id=id
+),
+temp_fat AS(
+    SELECT F.fdc_id, FN.amount*(amount/100) AS fat_value, FN.amount*CCF.fat_value*(amount/100) AS fat_calorie
+    FROM food F JOIN food_nutrient FN ON F.fdc_id = FN.fdc_id
+            JOIN nutrient N ON FN.nutrient_id = N.nutrient_id
+            JOIN food_nutrient_conversion_factor NCF ON F.fdc_id = NCF.fdc_id
+            JOIN food_calorie_conversion_factor CCF ON NCF.nutrient_conversion_id = CCF. nutrient_conversion_id
+    WHERE N.name = 'carbohydrate, by summation' AND F.fdc_id=id
+),
+temp_carb AS(
+    SELECT F.fdc_id, FN.amount*(amount/100) AS carbohydrate_value, FN.amount* CCF.carbohydrate_value*(amount/100) AS carbohydrate_calorie
+    FROM food F JOIN food_nutrient FN ON F.fdc_id = FN.fdc_id
+            JOIN nutrient N ON FN.nutrient_id = N.nutrient_id
+            JOIN food_nutrient_conversion_factor NCF ON F.fdc_id = NCF.fdc_id
+            JOIN food_calorie_conversion_factor CCF ON NCF.nutrient_conversion_id = CCF. nutrient_conversion_id
+    WHERE N.name = 'total fat (nlea)' AND F.fdc_id=id
+)
+SELECT TP.fdc_id, TP.protein_value, TF.fat_value, TC.carbohydrate_value, (TP.protein_calorie+TF.fat_calorie+TC.carbohydrate_calorie) AS calorie
+FROM temp_protein TP JOIN temp_fat TF ON TP.fdc_id = TF.fdc_id 
+    JOIN temp_carb TC ON TP.fdc_id = TC.fdc_id
+
+
+
+
+
+
+
+
+
+
+
+
+
